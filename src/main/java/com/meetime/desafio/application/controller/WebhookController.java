47407b44379a8d3e20328a836a3a209d40a8ee07
Davison Cavalcante <dcbarros1@gmail.com>
2025-04-04 19:25:25 -0300

    rebuild: Refatora o controller do webhooker removendo trechos não utilizados

--- src/main/java/com/meetime/desafio/application/controller/WebhookController.java
+++ src/main/java/com/meetime/desafio/application/controller/WebhookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.meetime.desafio.service.WebhookService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,23 +17,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebhookController {
 
-    private final WebhookService webhookService;
-
     @PostMapping
     public ResponseEntity<String> receiveWebhook(
-            @RequestBody String requestBody,
-            @RequestHeader(name = "X-HubSpot-Signature", required = false) String signature,
-            @RequestHeader(name = "X-HubSpot-Signature-Version", required = false) String signatureVersion) {
-
-        if (signature == null || signature.isEmpty()) {
-            return ResponseEntity.status(400).body("Missing X-HubSpot-Signature header");
-        }
-
-        boolean valid = webhookService.requestIsValid(requestBody, signature);
-        if (!valid) {
-            return ResponseEntity.status(403).body("Invalid signature");
-        }
-
+            @RequestBody JsonNode requestBody) {
         System.out.println("Webhook recebido: " + requestBody);
         return ResponseEntity.ok("Webhook processed");
     }
